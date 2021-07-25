d3b7a33ed10cab6028186845a03b0938296158f6
José Arthur <arthurlps123@gmail.com>
2021-07-24 23:15:34 -0300

    Update histV1.py

--- histV1.py
+++ histV1.py
@@ -1,12 +1,15 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+def histogramPlot(data, bins, width):
+    plt.hist(data, density=False, bins=bins, width=1)
+    plt.ylabel('Frequency')
+    plt.xlabel('Data');
+    plt.show()
+    
 #Classes = 11
 #Amplitude = 30-19 = 11 
 #Obtidos atraves do padrao de histograma
 
-x = [19, 19, 20, 20, 21, 22, 22, 23, 24, 25, 26, 26, 27, 29, 29, 29, 29, 30, 30, 30]
-plt.hist(x, density=False, bins=11, width=1)
-plt.ylabel('Frequency')
-plt.xlabel('Data');
-plt.show()
+data = [19, 19, 20, 20, 21, 22, 22, 23, 24, 25, 26, 26, 27, 29, 29, 29, 29, 30, 30, 30]
+histogramPlot(data, 11, 1)
