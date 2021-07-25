e66bad2d0137cc3fdacbf5b56d3cfc488225643c
José Arthur <arthurlps123@gmail.com>
2021-07-24 23:13:02 -0300

    Update HistogramV1.py

--- histV1.py
+++ histV1.py
@@ -1,13 +1,12 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-#Classes = 6
-#Amplitude = 23-18 = 5
-#Intervalo = 1.2
+#Classes = 11
+#Amplitude = 30-19 = 11 
 #Obtidos atraves do padrao de histograma
 
-x = [18,18,19,19,19,20,21,21,22,23]
-plt.hist(x, density=False, bins=6, width=1.2)
+x = [19, 19, 20, 20, 21, 22, 22, 23, 24, 25, 26, 26, 27, 29, 29, 29, 29, 30, 30, 30]
+plt.hist(x, density=False, bins=11, width=1)
 plt.ylabel('Frequency')
-plt.xlabel('Age');
+plt.xlabel('Data');
 plt.show()
